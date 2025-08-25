c05fa99c448ad04fdbbb50f0d42f4d20e46edc72
CristhianSZ <cristhiansosazurita@gmail.com>
2025-08-24 22:45:22 -0300

    feature-004:se garantiza la funcionalidad de tailwind y generan las rutas principales

---
+++ src/Views/Homepage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Homepage = () => {
+  return (
+    <div className='p-2 bg-blue-500 text-white hover:bg-red-700 flex justify-center'>Homepage</div>
+  )
+}
---
+++ src/Views/RegisterPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const RegisterPage = () => {
+  return (
+    <div className='p-2 bg-red-500 text-white hover:bg-green-700 flex justify-center'>RegisterPage</div>
+  )
+}
